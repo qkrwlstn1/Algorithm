915fa3b69cb37ae7094e1d7d41a74ff0e5b18b59
qkrwlstn1 <75283640+qkrwlstn1@users.noreply.github.com>
2023-10-13 16:41:50 +0900

    [level 2] Title: 피보나치 수, Time: 2.25 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public static int solution(int n) {
+        int[] D = new int[n + 1];
+        D[0] = 0;
+        D[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            D[i] = (D[i - 1] + D[i - 2]) % 1234567;
+        }
+
+        return D[n];
+    }
+}
