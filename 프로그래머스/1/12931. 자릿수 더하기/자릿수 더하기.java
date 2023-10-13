a31e8ea919a8bc4b5f639261b40b2283cf8b6525
qkrwlstn1 <75283640+qkrwlstn1@users.noreply.github.com>
2023-10-13 16:47:55 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        if(n < 10) return n;
+        while(n != 0) {
+        	answer += n % 10;
+        	n /= 10;
+        }
+
+        return answer;
+    }
+}
