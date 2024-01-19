59cfb16f6ad0241331ff6193dcb3d8b7adcecb44
qkrwlstn1 <75283640+qkrwlstn1@users.noreply.github.com>
2024-01-19 14:19:49 +0900

    [Silver V] Title: 🐜 기적의 매매법 🐜, Time: 76 ms, Memory: 11436 KB -BaekjoonHub

--- 백준/Silver/20546. 🐜 기적의 매매법 🐜/🐜 기적의 매매법 🐜.java
+++ 백준/Silver/20546. 🐜 기적의 매매법 🐜/🐜 기적의 매매법 🐜.java
@@ -1,66 +1,79 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int budget =Integer.parseInt(br.readLine());
-		int md[] = new int[14];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i=0; i<14; i++) {
-			md[i] = Integer.parseInt(st.nextToken());
-		}
-			
-		int nbp = nbp(budget,md);
-		int timing = timing(budget,md);
-		
-		if(nbp > timing) bw.append("BNP");
-		else if(nbp < timing) bw.append("TIMING");
-		else bw.append("SAMESAME");
-		
-		bw.append("");
-		bw.close();
-		br.close();
-		
-		
-	}
-	
-	static int nbp(int budget, int[] md) {
-		int cnt=0;
-		for(int i=0;i<14;i++) {
-			if(budget >= md[i]) {
-				cnt += budget/md[i];
-				budget = budget%md[i];
-			}
-		}
-		return budget + (cnt * md[13]);
-	}
-	
-	static int timing(int budget, int[] md) {
-		int cnt=0;
-		int upDay=1;
-		int downDay=0;
-		for(int i=1;i<14;i++) {
-			if(md[i]>md[i-1]) upDay++;
-			else upDay =0;
-			
-			if(md[i]<md[i-1]) downDay++;
-			else downDay=0;
-			
-			
-			if(upDay == 3) {
-				budget += md[i] * cnt;
-				cnt=0;
-			}else if(downDay>2) {
-				cnt += budget/md[i];
-				budget = budget%md[i];
-			}
-		}
-		return budget + (cnt * md[13]);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int cash = Integer.parseInt(st.nextToken()); // 자본금 입력
+
+        int JH_money = cash; // 준현 최종금액
+        int SM_money = cash; // 성민 최종금액
+
+        int up = 0; // 연속 상승
+        int down = 0; // 연속 하락
+
+        int JH_joo = 0; // 준현 주식 개수
+        int SM_joo = 0; // 성민 주식 개수
+
+        int[] arr = new int[14];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 14; i++) {
+            arr[i] = Integer.parseInt(st.nextToken()); // 주가 입력
+        }
+
+        // 현준의 최종 금액
+        for (int i = 0; i < 14; i++) {
+            if (cash >= arr[i]) {
+                JH_joo += JH_money / arr[i];
+                JH_money = JH_money % arr[i];
+               
+            }
+        }
+
+        SM_money = cash; // 성민 자본금
+
+        // 성민의 최종 금액
+        for (int i = 0; i < 14; i++) {
+            if (i == 0) {
+                continue;
+            } else {
+                if (arr[i] < arr[i - 1]) { // 전일 대비 하락 시
+                    down++;
+                    up = 0;
+                } else if (arr[i] > arr[i - 1]) { // 전일 대비 상승 시
+                    up++;
+                    down = 0;
+                } else { // 동일할 시
+                    up = 0;
+                    down = 0;
+                }
+
+                // 매수, 매도
+                if (SM_money >= arr[i]) { // 보유 현금이 더 많아야 매수
+                    if (down >= 3) { // 3일 연속 하락인 경우
+                        SM_joo += SM_money / arr[i];
+                        SM_money %= arr[i];
+                    }
+                }
+                if (up >= 3) { // 3일 연속 상승인 경우
+                    SM_money += SM_joo * arr[i];
+                    SM_joo = 0;
+                }
+            }
+        }
+
+        int SM_final = SM_money + (arr[13] * SM_joo);
+        int JH_final = JH_money + (arr[13] * JH_joo);
+
+        if (SM_final > JH_final) {
+            System.out.println("TIMING");
+        } else if (JH_final > SM_final) {
+            System.out.println("BNP");
+        } else {
+            System.out.println("SAMESAME");
+        }
+    }
 }
