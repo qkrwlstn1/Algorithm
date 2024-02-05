516405f24e3a747ab8401f298a5717a7ee619381
qkrwlstn1 <75283640+qkrwlstn1@users.noreply.github.com>
2024-02-05 15:12:03 +0900

    [Silver IV] Title: 괄호, Time: 80 ms, Memory: 11876 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,23 +1,50 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
-public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int loop = Integer.parseInt(sc.nextLine());
-		int cnt;
-		loop : for(int i=0; i<loop; i++) {
-			String vps = sc.nextLine();
-			cnt=0;
-			for(int j=0; j<vps.length(); j++) {
-				if(vps.charAt(j) == '(') cnt++;
-				else cnt--;
-				if(cnt <0) {
-					System.out.println("NO");
-					continue loop;
-				}
-			}
-			if(cnt !=0) System.out.println("NO");
-			else System.out.println("YES");
+public class Main{
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+	
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int front=0, rear=0;
+		int n = Integer.parseInt(br.readLine());
+		
+		
+		for(int i =0; i<n; i++) {
+			
+			bw.append(vps(br.readLine())+"\n");
+			
 		}
+		
+		bw.flush();
+		bw.close();
+		br.close();
 	}
+	
+	static String vps(String str) {
+		Stack<String> st = new Stack<String>();	
+		for(int i=0; i<str.length();i++) {
+			if(str.charAt(i) == '(') st.push("(");
+			else {
+				if(st.empty()) {
+					return "NO";
+				}
+				st.pop();
+			}
+		}
+		
+		if(st.empty()) return "YES";
+		
+		return "NO";
+		
+	}
+
 }
