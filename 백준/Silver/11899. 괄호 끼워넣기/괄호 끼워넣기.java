e80ccb55cf19db8e04f7f3e686c7822ee498bd43
qkrwlstn1 <75283640+qkrwlstn1@users.noreply.github.com>
2024-02-07 14:42:12 +0900

    [Silver III] Title: 괄호 끼워넣기, Time: 80 ms, Memory: 11640 KB -BaekjoonHub

---
+++ 백준/Silver/11899. 괄호 끼워넣기/괄호 끼워넣기.java
@@ -0,0 +1,44 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int answer =0;
+		Stack<Character> stack = new Stack<Character>();
+		int right =0;
+		String s = st.nextToken();
+		int sLen = s.length();
+		
+		for(int i=0; i<sLen; i++) {
+			stack.add(s.charAt(i));
+		}
+		for(int i=0; i<sLen; i++) {
+			if(stack.pop() == ')') {
+				right++;
+			}else {
+				if(right>0) right--;
+				else answer++;
+			}
+		}
+		answer += right;
+		
+		bw.append(answer + "");
+		bw.close();
+		br.close();
+
+	}
+
+}
