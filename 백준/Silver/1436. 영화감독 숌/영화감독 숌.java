8b3d3253e255f2e0f802f09bfd736631e63d04f6
qkrwlstn1 <75283640+qkrwlstn1@users.noreply.github.com>
2024-02-08 14:54:47 +0900

    [Silver V] Title: 영화감독 숌, Time: 328 ms, Memory: 113116 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int answer =666;
+		int b = Integer.parseInt(st.nextToken());
+		int cnt=0;
+		while(cnt != b) {
+			if((answer+"").contains("666")) cnt++;
+			answer++;
+		}
+		bw.append(--answer + "");
+		bw.close();
+		br.close();
+
+	}
+
+}
