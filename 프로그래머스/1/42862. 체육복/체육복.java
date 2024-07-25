a6e6a849a20c96d31a2b14259b9f3a503a9b99da
qkrwlstn1 <75283640+qkrwlstn1@users.noreply.github.com>
2024-07-25 14:54:37 +0900

    [level 1] Title: 체육복, Time: 0.57 ms, Memory: 69.9 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,24 +1,22 @@
 import java.util.Arrays;
 
 public class Solution {
-    public static int solution(int n, int[] lost, int[] reserve) {
+    public int solution(int n, int[] lost, int[] reserve) {
         int answer =0;
         boolean[] lend = new boolean[n+2];
         for (int i : reserve) {
             lend[i] = true;
         }
         Arrays.sort(lost);
-
-        for (int i = 0; i < lost.length; i++) {
-            if(lend[lost[i]]){
+        Arrays.sort(reserve);
+        for (int i : lost) {
+            if(lend[i]){
                 answer++;
-                lend[lost[i]] = false;
-                lost[i] = -1;
+                lend[i] = false;
             }
         }
 
         for (int i = 0; i < lost.length; i++) {
-            if(lost[i] == -1) continue;
             if(lend[lost[i]-1]){
                 answer++;
                 lend[lost[i]-1] = false;
@@ -28,11 +26,5 @@ public class Solution {
             }
         }
         return n-(lost.length-answer);
-    }
-
-    public static void main(String[] args) {
-        int[] lost = new int[]{3,5};
-
-        System.out.println(solution(5,new int[]{5,3}, new int[]{4,2}));
     }
 }
