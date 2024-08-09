c6c4ce43e351a66d089dc425ed85f4e5770fcab9
qkrwlstn1 <75283640+qkrwlstn1@users.noreply.github.com>
2024-08-09 17:38:37 +0900

    [Bronze II] Title: Hashing, Time: 64 ms, Memory: 11608 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        long answer = 0;
+        long tmp =1;
+        HashMap<Character,Integer> map = new HashMap<>();
+        for (int i = 0; i < 26; i++) {
+            map.put((char)('a' + i), i+1);
+        }
+        char[] arr = br.readLine().toCharArray();
+        for (int i = 0; i < n; i++) {
+            answer += tmp * map.get(arr[i]);
+            tmp = tmp * 31 % 1234567891;
+        }
+
+        bw.append(answer+"");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+
+}
