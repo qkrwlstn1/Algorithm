6b326943d4983c17039b0595894798a4a58b3a94
qkrwlstn1 <75283640+qkrwlstn1@users.noreply.github.com>
2025-06-24 14:56:26 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 72 ms, Memory: 11988 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
@@ -1,29 +1,35 @@
-
 import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
-import static java.lang.Integer.*;
-
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
+    static int n;
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        //StringTokenizer st = new StringTokenizer(br.readLine());
-        int t = parseInt(br.readLine());
-        int c;
-        for (int i = 0; i < t; i++) {
-            c= parseInt(br.readLine());
-            bw.append(c / 25 + " ");
-            c %= 25;
-            bw.append(c/10+" ");
-            c %= 10;
+
+        StringTokenizer st;
+
+        n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int c = Integer.parseInt(br.readLine());
+            bw.append(c/25 + " ");
+            c = c % 25;
+            bw.append(c/10 + " ");
+            c = c % 10;
             bw.append(c/5 + " ");
-            bw.append(c%5 + "\n");
+            c = c % 5;
+            bw.append(c + "\n");
         }
 
+        bw.flush();
         bw.close();
-        br.close();
     }
+
 
 }
