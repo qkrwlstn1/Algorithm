8b2b8904d81db70592b47d7d55cf5cfe2faf4ec6
박진수 <75283640+qkrwlstn1@users.noreply.github.com>
2025-08-07 16:48:53 +0900

    [Silver III] Title: 계단 오르기, Time: 68 ms, Memory: 11720 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,49 +1,36 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.StringTokenizer;
 
-import javax.net.ssl.SSLContext;
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
-	static int stairs[];
-	static int arr[];
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		int n = Integer.parseInt(br.readLine());
-		stairs = new int[n];
-		arr = new int[n];
-		for(int i=0; i<n; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-		}
-		stairs[0] = arr[0];
-		if(n == 1 ) {
-			System.out.println(arr[0]);
-			return;
-		}
-		stairs[1] = arr[0] + arr[1];
-		if(n == 2) {
-			System.out.println(stairs[1]);
-			return;
-		}
-		stairs[2] = arr[2]+Math.max(arr[0],arr[1]);
-		if(n == 3) {
-			System.out.println(stairs[2]);
-			return;
-		}
-		bw.append(dp(n-1)+"");
-		bw.flush();
-		bw.close();
-		br.close();
-	}
-	
-	static int dp(int index) {
-		if(stairs[index] > 0) return stairs[index];
-		
-		return stairs[index] = Math.max(dp(index-3) + arr[index] +arr[index-1],dp(index-2)+arr[index]);
-		
-	}
+    static int n;
+    static int[] arr, dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
+        arr = new int[Math.max(n+1, 3)];
+        dp = new int[Math.max(n+1, 3)];
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            dp[i] = -1;
+        }
+        dp[1] = arr[1];
+        dp[2] = arr[1] + arr[2];
+
+        bw.append(dp(n) + "");
+        bw.flush();
+        bw.close();
+    }
+
+    static int dp(int num){
+        if(dp[num] != -1) return dp[num];
+        return dp[num] =  Math.max(dp(num-3) + arr[num-1], dp(num-2)) + arr[num];
+    }
+
+
 }
